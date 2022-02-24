f045188ab64d8d65faf9f3f6f8b52aa107c6bc8b
Hayden <boozerhaydens@gmail.com>
2022-02-24 12:54:54 -0500

    Adding run command with java doc

--- RunCommand.java
+++ RunCommand.java
@@ -3,18 +3,27 @@ import java.io.FileReader;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * Command class that executes the run animation in the console
  * @author Hayden Boozer
  * 
  */
 public class RunCommand implements Command {
+    /**
+     * Default Constructor: Sets the player to the current player
+     * @param player The player that is passed in
+     * 
+     */
     RunCommand(Player player) {
         this.player = player;
     }
 
+    /**
+     * Main method of the class that reads the txt file, and prints the necessary lines to perform the console animation
+     * 
+     */
     public void execute() {
         int line = 0;
         try {
-            String currentLine;
             BufferedReader reader = new BufferedReader(new FileReader("./run.txt"));
             while(reader.readLine() != null) {
                 if (line % 3 == 0) {
@@ -25,11 +34,17 @@ public class RunCommand implements Command {
                 }
                 System.out.println(reader.readLine());
             }
+            reader.close();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
+    /**
+     * Sleeps the loop for a temporary time
+     * @param num The amount of times that the sleep happens
+     * 
+     */
     private void sleep(int num) {
         try {
             TimeUnit.MILLISECONDS.sleep(num);
@@ -38,6 +53,10 @@ public class RunCommand implements Command {
         }
     }
     
+    /**
+     * Clears the console.
+     * 
+     */
     private void clear() {
         System.out.print("\033[H\033[2J");
     }
