44a3eaef61b3f89398ec863147669ba9c2f09f04
PierceClinger <70237555+PierceClinger@users.noreply.github.com>
2022-02-26 13:17:02 -0500

    Edited InputHandler

--- InputHandler.java
+++ InputHandler.java
@@ -2,17 +2,16 @@ import java.util.HashMap;
 
 public class InputHandler {
     private HashMap<String, command> commands = new HashMap<String, command>();
-    private Player player;
 
     public InputHandler(Player player) {
         this.player = player;
-        commands.put("jump", JumpCommand);
-        commands.put("run", RunCommand);
-        commands.put("fire", FireCommand);
-        commands.put("quit", QuitCommand);
+        commands.put("jump", JumpCommand());
+        commands.put("run", RunCommand());
+        commands.put("fire", FireCommand());
+        commands.put("quit", QuitCommand());
     }
 
     public void buttonPressed(String button) {
-        commands.get(button).run();
+        commands.get(button).execute();
     }
 }
