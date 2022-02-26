59f5b18905978f9bc8228263da14889420232eda
Hbunce1561 <hbunce1561@gmail.com>
2022-02-26 13:15:45 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,19 @@
+
 public class Player {
-    
+	public Player() {
+		
+	}
+
+public void jump() {
+	command jC = new JumpCommand(this);
+	jC.execute();
+}
+public void fire() {
+	command fC = new FireCommand(this);
+	fC.execute();
+}
+public void runForward() {
+	command rC = new RunCommand(this);
+	rC.execute();
+}
 }
